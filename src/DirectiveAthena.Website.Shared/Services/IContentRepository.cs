7ef8c2f5ba7e6b9d4d0c412a9d1952d50fa6183a
Anna Sas <anna.sas.dev@gmail.com>
2026-01-24 14:02:46 +0100

    Add `ContentBase` class and `IContentRepository` interface for unified content management
    - Introduced `ContentBase` abstract class with shared properties and helper methods for content handling.
    - Added `IContentRepository` interface to standardize repository operations for `ContentBase`-derived types.

--- src/DirectiveAthena.Website.Shared/Services/IContentRepository.cs
+++ src/DirectiveAthena.Website.Shared/Services/IContentRepository.cs
@@ -1,11 +1,15 @@
-﻿// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
 namespace DirectiveAthena.Website.Services;
-
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
-public interface ICachedJsonRepository<T> {
+public interface IContentRepository<T> where T : ContentBase {
+    ValueTask<T[]> GetAllAsync(CancellationToken ct = default);
+    ValueTask<T?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    ValueTask<bool> DeleteByIdAsync(Guid id, CancellationToken ct = default);
+    
     ValueTask<bool> SaveAsync(IEnumerable<T> items, CancellationToken ct = default);
+    ValueTask<string> GetAsJsonStringAsync(CancellationToken ct = default);
 }
