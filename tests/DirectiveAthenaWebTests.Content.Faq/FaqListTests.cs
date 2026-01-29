605a00eedae619a798a9b4daa3ea487b271d61cd
Anna Sas <anna.sas.dev@gmail.com>
2026-01-29 23:03:04 +0100

    Add localization services and options to Faq and Note component tests
    - Introduced `ILocalizationProvider` and `LocalizationOptions` to Faq and Note tests.
    - Configured localization services with support for "en" and "nl" languages.

--- tests/DirectiveAthenaWebTests.Content.Faq/FaqListTests.cs
+++ tests/DirectiveAthenaWebTests.Content.Faq/FaqListTests.cs
@@ -4,9 +4,11 @@
 using Bunit;
 using DirectiveAthenaWeb.Content.Faq;
 using DirectiveAthenaWeb.Content.Faq.Components;
+using DirectiveAthenaWeb.Services.Localization;
 using DirectiveAthenaWeb.Services.Localization.Resources;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
 using MudBlazor.Services;
 
 namespace DirectiveAthenaWebTests.Content.Faq;
@@ -20,6 +22,11 @@ public class FaqListTests {
         await using var ctx = new BunitContext();
         ctx.Services.AddMudServices();
         ctx.Services.AddLocalization();
+        ctx.Services.AddLogging();
+        ctx.Services.AddSingleton(Options.Create(new DirectiveAthenaWeb.Services.Localization.LocalizationOptions()
+            .AddLocalization("en", "English", "EN", "https://flagcdn.com/w40/us.png")
+            .AddLocalization("nl", "Nederlands", "NL", "https://flagcdn.com/w40/nl.png")));
+        ctx.Services.AddSingleton<ILocalizationProvider, LocalizationProvider>();
         ctx.JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);
         var localizer = ctx.Services.GetRequiredService<IStringLocalizer<Shared>>();
 
--- tests/DirectiveAthenaWebTests.Content.Note/NoteListTests.cs
+++ tests/DirectiveAthenaWebTests.Content.Note/NoteListTests.cs
@@ -3,9 +3,11 @@
 // ---------------------------------------------------------------------------------------------------------------------
 using Bunit;
 using DirectiveAthenaWeb.Content.Note;
+using DirectiveAthenaWeb.Services.Localization;
 using DirectiveAthenaWeb.Services.Localization.Resources;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
 using MudBlazor.Services;
 
 namespace DirectiveAthenaWebTests.Content.Note;
@@ -19,6 +21,11 @@ public class NoteListTests {
         await using var ctx = new BunitContext();
         ctx.Services.AddMudServices();
         ctx.Services.AddLocalization();
+        ctx.Services.AddLogging();
+        ctx.Services.AddSingleton(Options.Create(new DirectiveAthenaWeb.Services.Localization.LocalizationOptions()
+            .AddLocalization("en", "English", "EN", "https://flagcdn.com/w40/us.png")
+            .AddLocalization("nl", "Nederlands", "NL", "https://flagcdn.com/w40/nl.png")));
+        ctx.Services.AddSingleton<ILocalizationProvider, LocalizationProvider>();
         ctx.JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);
         var localizer = ctx.Services.GetRequiredService<IStringLocalizer<Shared>>();
 
