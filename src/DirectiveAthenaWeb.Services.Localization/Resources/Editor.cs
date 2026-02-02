799594407ade550aa9c05b342373547e1ea2f8d3
Anna Sas <anna.sas.dev@gmail.com>
2026-02-02 14:10:46 +0100

    Extract `Editor` localization resources into a dedicated file and refactor `ContentEditor` to use `EditorLocalizer` for improved modularity and maintainability. Remove unused `Shared` keys related to `Editor`.

---
+++ src/DirectiveAthenaWeb.Services.Localization/Resources/Editor.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+namespace DirectiveAthenaWeb.Services.Localization.Resources;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class Editor {
+    public const string EditItem = "EditItem";
+    public const string UniqueId = "UniqueId";
+    public const string IdHelperItem = "IdHelperItem";
+    public const string CreatedAtLabel = "CreatedAtLabel";
+    public const string LastModifiedAtLabel = "LastModifiedAtLabel";
+    public const string HiddenLabel = "HiddenLabel";
+    public const string MarkdownFileLabel = "MarkdownFileLabel";
+    public const string MarkdownFileHelper = "MarkdownFileHelper";
+    public const string TagsTitle = "TagsTitle";
+    public const string AddTag = "AddTag";
+    public const string TagsHint = "TagsHint";
+
+    public const string MarkdownLabel = "MarkdownLabel";
+    public const string MarkdownStubsSaveDisk = "MarkdownStubsSaveDisk";
+    public const string MarkdownStubsDownload = "MarkdownStubsDownload";
+    public const string DeleteItemHardConfirm = "DeleteItemHardConfirm";
+    public const string DeleteItemSoftConfirm = "DeleteItemSoftConfirm";
+    public const string DeleteItemSoftSuccess = "DeleteItemSoftSuccess";
+    public const string DeleteItemSoftFailed = "DeleteItemSoftFailed";
+    public const string DeleteItemHardSuccess = "DeleteItemHardSuccess";
+    public const string DeleteItemHardFailed = "DeleteItemHardFailed";
+}
